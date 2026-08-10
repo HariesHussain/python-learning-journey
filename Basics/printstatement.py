487dada704b1dfb12d83b84b45d3ba6359dd9dd5
Haries Hussain <shaikharieshussain09@gmail.com>
2026-08-10 21:19:07 +0530

    Day 1: Hello World, print statements, escape sequences

---
+++ Basics/printstatement.py
@@ -0,0 +1,4 @@
+print("'Hello World!'")  # prints Hello world
+print("Hello I am Haries Hussain /n I am an B-tech third year student in Santhiram Engineering college Nandyal.")  # prints Hello I am Haries Hussain /n I am an B-tech third year student
+print(365*900)
+print( "Hello my name is \" Haries Hussain \" ")

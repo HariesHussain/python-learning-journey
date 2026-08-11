66181b17a4add715e06538d657a623666660a2a8
Haries Hussain <shaikharieshussain09@gmail.com>
2026-08-11 20:14:25 +0530

    Variables and Data Types

---
+++ Basics/variables.py
@@ -0,0 +1,21 @@
+# variables and data types
+
+a = 3 #int
+b = "Haries Hussain"  #string
+c = True  #boolean
+d = 9 
+print (d*a)
+e = 8.9  # float
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+list1 = [2 , 3, ["Haries","Hussain"]]
+print(list1)
+tuple1 = (("Tiger","LION") , ("horse", "cheetah"))
+print(tuple1)
+
+dist1 = {"Name" : "Haries" , "Age" : 19 , "can vote" : "yes"}
+print(dist1)
